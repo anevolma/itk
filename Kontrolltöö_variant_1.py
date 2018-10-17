9ca0b71eba84a309a9399a8c23fc5aca012a8182
Annika Kaskma <Annika.Kaskma@energia.ee>
2018-10-17 17:09:04 +0300

    Kommenteeritud töö

--- Kontrolltöö_variant_1.py
+++ Kontrolltöö_variant_1.py
@@ -1,39 +1,44 @@
-#Ülesanne 1
+# #Ülesanne 1
 
-def list_sum(list):
-    """this function takes an integer and returns sum of 1+2+...+N"""
+# def list_sum(list):
+    # """this function takes an integer and returns sum of 1+2+...+N"""
     
-    result = 0   
-    for item in range(len(list)):
-        result = result + list[item]
-    return(result)
+    # result = 0   
+    # for item in range(len(list)):
+        # result = result + list[item]
+    # return(result)
 
-print(list_sum([3, 5, 1]))
+# print(list_sum([3, 5, 1]))
+# # Väga hea, tubli töö ;)
 
-#Ülesanne 2
+# #Ülesanne 2
 
-def liblikas(string):
-    string = string.lower()
-    tekst = string.split()
-    for item in range(len(tekst)-1, 0, -1):
-        if tekst[item].startswith('a'):
-            tekst[item] = 'liblikas'
-            return(tekst)
-            break
+# def liblikas(string):
+    # string = string.lower()
+    # tekst = string.split()
+    # for item in range(len(tekst)-1, 0, -1):  #alusta listi viimasest elemendist! kolmene range  -1 tagurpidi üks
+        # if tekst[item].startswith('a'):
+            # tekst[item] = 'liblikas'
+            # return(tekst)
+            # break
     
-print(liblikas("Kui Arno isaga koolimajja jõudis, olid tunnid juba alanud"))
+# print(liblikas("Kui Arno isaga koolimajja jõudis, olid tunnid juba alanud"))
+# # Väga hea, tubli töö ;)
 
-#Ülesanne 3
+
+# #Ülesanne 3
         
 def common_symbols(string, string1):
     result = 0
     result1 = 0
     list = []
+	# 
     for i in range(len(string)):
         if string[i] == string1[i]:
             result = result + 1
     result = str(result)
     
+	#nested for kahe stringi võrdlus
     for i in range(len(string)):
         for j in range(len(string1)):
             if string[i] == string1[j]:
@@ -44,7 +49,8 @@ def common_symbols(string, string1):
     result1 = str(len(list))
     vastus = result + "," + result1
     print(vastus)
-           
+
+	
 common_symbols("TAOA", "TUBA")
-            
+# Väga hea, tubli töö ;)      
 print("Tere!")
